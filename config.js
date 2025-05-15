133aa27e9592f0ad300dd3bfc9bc661ea9775e73
madalinaradu1 <madalina.radu1@gcu.edu>
2025-05-15 13:56:43 -0700

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,8 @@ window._config = {
     userPoolId: 'us-east-1_JHnm7JteW',
     userPoolClientId: '6m2e6sh0hbnqtp0cm8b9nrvaiv',
     region: 'us-east-1'
-  }
+   },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
 };
