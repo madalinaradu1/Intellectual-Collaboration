8ece20806529251fc4427efee59489a90e019f69
madalinaradu1 <madalina.radu1@gcu.edu>
2025-05-22 11:02:26 -0700

    Updating code

--- amplify-config.js
+++ amplify-config.js
@@ -1,19 +1,23 @@
 // Amplify configuration
-const awsConfig = {
-  Auth: {
-    region: _config.cognito.region,
-    userPoolId: _config.cognito.userPoolId,
-    userPoolWebClientId: _config.cognito.userPoolClientId
-  },
-  API: {
-    endpoints: [
-      {
-        name: "icApi",
-        endpoint: _config.api.invokeUrl
-      }
-    ]
-  }
-};
+if (typeof _config !== 'undefined') {
+  const awsConfig = {
+    Auth: {
+      region: _config.cognito.region,
+      userPoolId: _config.cognito.userPoolId,
+      userPoolWebClientId: _config.cognito.userPoolClientId
+    },
+    API: {
+      endpoints: [
+        {
+          name: "icApi",
+          endpoint: _config.api.invokeUrl
+        }
+      ]
+    }
+  };
 
-// Export for use with Amplify
-window.awsConfig = awsConfig;
+  // Export for use with Amplify
+  window.awsConfig = awsConfig;
+} else {
+  console.error("_config is not defined in amplify-config.js");
+}
