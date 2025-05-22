e476bb27e6618385b96b3603ca5550683842d671
madalinaradu1 <madalina.radu1@gcu.edu>
2025-05-22 11:17:43 -0700

    Updating amplify credentials

--- amplify-config.js
+++ amplify-config.js
@@ -2,15 +2,26 @@
 if (typeof _config !== 'undefined') {
   const awsConfig = {
     Auth: {
+      // Required configuration
       region: _config.cognito.region,
       userPoolId: _config.cognito.userPoolId,
-      userPoolWebClientId: _config.cognito.userPoolClientId
+      userPoolWebClientId: _config.cognito.userPoolClientId,
+      
+      // Additional required settings
+      authenticationFlowType: 'USER_SRP_AUTH',
+      mandatorySignIn: true
     },
     API: {
       endpoints: [
         {
           name: "icApi",
-          endpoint: _config.api.invokeUrl
+          endpoint: _config.api.invokeUrl,
+          region: _config.cognito.region,
+          custom_header: async () => {
+            return { 
+              Authorization: (await Amplify.Auth.currentSession()).getIdToken().getJwtToken()
+            }
+          }
         }
       ]
     }
