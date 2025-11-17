8938f13f9ed7e00c07787c10cc8c1da895049493
Madalina Radu <madalina.radu1@gcu.edu>
2025-11-17 15:49:51 -0700

    Adding a login page functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Amplify } from 'aws-amplify';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import awsExports from './aws-exports';
 import './App.css';
+
+Amplify.configure(awsExports);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <div className="App">
+          <header className="App-header">
+            <h1>Welcome, {user.username}!</h1>
+            <button onClick={signOut}>Sign out</button>
+          </header>
+        </div>
+      )}
+    </Authenticator>
   );
 }
 
